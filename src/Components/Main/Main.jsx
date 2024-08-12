34c0a155b4342fc289d8da059ccff0671e36382b
Agata Kamińska <a.fluffypie@gmail.com>
2024-08-12 13:07:14 +0200

    feat: renamed the OutputContainer component to ProjectContainerLight, added styles and imports for the component; fix: changed the order of css style imports so that bootstrap styles do not write component styles;

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,8 +1,11 @@
 import "./Main.css";
+import ProjectContainerLight from "../ProjectContainerLight/ProjectContainerLight";
 
 const Main = () => {
   return (
-    <main className="d-flex justify-content-center main__container"></main>
+    <main className="d-flex justify-content-center main__container">
+        <ProjectContainerLight/>
+    </main>
   );
 };
 export default Main;
---
+++ src/Components/ProjectContainerLight/ProjectContainerLight.jsx
@@ -0,0 +1,8 @@
+import "./ProjectContainerLight.css";
+
+const ProjectContainerLight = () => {
+    return (
+        <section className="d-flex justify-content-center align-items-end justify-content-xl-start project__container--light"></section>
+    )
+}
+export default ProjectContainerLight;
