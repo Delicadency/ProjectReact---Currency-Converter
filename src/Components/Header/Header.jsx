f1d9f04400207d2f4283fb4d08bc4dafd4c317b8
Agata Kamińska <164206709+Delicadency@users.noreply.github.com>
2024-08-12 19:42:26 +0000

    created and imported Header component

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="d-flex align-items-center justify-content-center header__container"></header>
+  );
+};
+export default Header;
--- src/Components/ProjectContainer/ProjectContainer.jsx
+++ src/Components/ProjectContainer/ProjectContainer.jsx
@@ -1,8 +1,11 @@
 import "./ProjectContainer.css";
+import Header from "../Header/Header";
 
 const ProjectContainer = () => {
   return (
-    <section className="d-flex container-fluid flex-column align-items-center project__container"></section>
+    <section className="d-flex container-fluid flex-column align-items-center project__container">
+      <Header />
+    </section>
   );
 };
 export default ProjectContainer;
