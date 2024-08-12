4ccc0c6e6bcc3e835dc9492a25a6a4805af878a1
Agata Kamińska <164206709+Delicadency@users.noreply.github.com>
2024-08-12 18:39:24 +0000

    feat: created and imported OutputContainer component

---
+++ src/Components/OutputContainer/OutputContainer.jsx
@@ -0,0 +1,8 @@
+import "./OutputContainer.css";
+
+const OutputContainer = () => {
+  return (
+    <section className="d-flex justify-content-center align-items-center justify-content-xl-start output__container"></section>
+  );
+};
+export default OutputContainer;
--- src/Components/ProjectContainerLight/ProjectContainerLight.jsx
+++ src/Components/ProjectContainerLight/ProjectContainerLight.jsx
@@ -1,8 +1,11 @@
 import "./ProjectContainerLight.css";
+import OutputContainer from "../OutputContainer/OutputContainer";
 
 const ProjectContainerLight = () => {
-    return (
-        <section className="d-flex justify-content-center align-items-end justify-content-xl-start project__container--light"></section>
-    )
-}
+  return (
+    <section className="d-flex justify-content-center align-items-end justify-content-xl-start project__container--light">
+      <OutputContainer />
+    </section>
+  );
+};
 export default ProjectContainerLight;
