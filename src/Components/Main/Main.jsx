b7532cb95cad9cac5d333dcd96c23178ba9f445c
Agata Kamińska <164206709+Delicadency@users.noreply.github.com>
2024-08-12 19:35:37 +0000

    feat: created and imported ProjectContainer component

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,12 +1,13 @@
 import "./Main.css";
 import ProjectContainerLight from "../ProjectContainerLight/ProjectContainerLight";
 import SpinnerContainer from "../SpinnerContainer/SpinnerContainer";
-
+import ProjectContainer from "../ProjectContainer/ProjectContainer";
 const Main = () => {
   return (
     <main className="d-flex justify-content-center main__container">
       <ProjectContainerLight />
       <SpinnerContainer />
+      <ProjectContainer />
     </main>
   );
 };
---
+++ src/Components/ProjectContainer/ProjectContainer.jsx
@@ -0,0 +1,8 @@
+import "./ProjectContainer.css";
+
+const ProjectContainer = () => {
+  return (
+    <section className="d-flex container-fluid flex-column align-items-center project__container"></section>
+  );
+};
+export default ProjectContainer;
