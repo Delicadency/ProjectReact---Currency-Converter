3cdb741d63c09e03778fda3846355b78f38410c1
Agata Kamińska <164206709+Delicadency@users.noreply.github.com>
2024-08-12 19:47:22 +0000

    created and imported Heading component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,11 @@
 import "./Header.css";
+import Heading from "../Heading/Heading";
 
 const Header = () => {
   return (
-    <header className="d-flex align-items-center justify-content-center header__container"></header>
+    <header className="d-flex align-items-center justify-content-center header__container">
+      <Heading />
+    </header>
   );
 };
 export default Header;
---
+++ src/Components/Heading/Heading.jsx
@@ -0,0 +1,6 @@
+import "./Heading.css";
+
+const Heading = () => {
+  return <h1 className="header__heading">Przelicznik Walut</h1>;
+};
+export default Heading;
