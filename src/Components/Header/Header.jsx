b222c43a960aa1b7d7021704ece01c102d7f0b60
Agata Kamińska <a.fluffypie@gmail.com>
2024-08-13 12:17:36 +0200

    feat: created and imported Logo component; refactor: renamed folders

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import "./Header.css";
 import Heading from "../Heading/Heading";
+import Logo from "../Logo/Logo";
 
 const Header = () => {
   return (
     <header className="d-flex align-items-center justify-content-center header__container">
+      <Logo />
       <Heading />
     </header>
   );
---
+++ src/Components/Logo/Logo.jsx
@@ -0,0 +1,6 @@
+import "./Logo.css";
+import logo from "../../images/logo.png"
+const Logo = () => {
+  return <img className="header__logo" src={logo} alt="piggybank logo" />;
+};
+export default Logo;
