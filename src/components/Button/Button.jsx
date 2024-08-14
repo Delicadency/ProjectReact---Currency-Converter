e7c5439b1f542aeecd2c5e655e46a4561259e09e
Agata Kamińska <a.fluffypie@gmail.com>
2024-08-14 12:18:29 +0200

    feat: created and imported Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+import "./Button.css";
+
+const Button = ({ currency }) => {
+  return (
+    <button className="d-flex align-items-center justify-content-center bar__button"></button>
+  );
+};
+export default Button;
--- src/components/ButtonBar/ButtonBar.jsx
+++ src/components/ButtonBar/ButtonBar.jsx
@@ -1,8 +1,13 @@
 import "./ButtonBar.css";
+import Button from "../Button/Button";
 
 const ButtonBar = () => {
   return (
-    <section className="d-flex align-items-center justify-content-center bar"></section>
+    <section className="d-flex align-items-center justify-content-center bar">
+      <Button currency="USD" />
+      <Button currency="EUR" />
+      <Button currency="CHF" />
+    </section>
   );
 };
 export default ButtonBar;
