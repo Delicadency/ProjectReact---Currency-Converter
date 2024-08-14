1b2f18804ab2e34fdb7c7cc601180537f59cdccc
Agata Kamińska <a.fluffypie@gmail.com>
2024-08-14 12:47:06 +0200

    feat: created and imported InputSection component

---
+++ src/components/InputSection/InputSection.jsx
@@ -0,0 +1,11 @@
+import "./InputSection.css";
+import InputContainer from "../InputContainer/InputContainer";
+
+const InputSection = () => {
+  return (
+    <section className="section__container d-flex flex-column flex-xl-row align-items-center justify-content-xl-between align-items-xl-center">
+      <InputContainer />
+    </section>
+  );
+};
+export default InputSection;
--- src/components/ProjectContainer/ProjectContainer.jsx
+++ src/components/ProjectContainer/ProjectContainer.jsx
@@ -1,12 +1,14 @@
 import "./ProjectContainer.css";
 import Header from "../Header/Header";
 import ButtonBar from "../ButtonBar/ButtonBar";
+import InputSection from "../InputSection/InputSection";
 
 const ProjectContainer = () => {
   return (
     <section className="d-flex container-fluid flex-column align-items-center project__container">
       <Header />
       <ButtonBar />
+      <InputSection />
     </section>
   );
 };
