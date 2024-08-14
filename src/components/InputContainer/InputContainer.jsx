a78565d2a2c093d9225bb1c92b1bfcd823d9cab5
Agata Kamińska <a.fluffypie@gmail.com>
2024-08-14 12:41:16 +0200

    feat: created InputContainer component

---
+++ src/components/InputContainer/InputContainer.jsx
@@ -0,0 +1,8 @@
+import "./InputContainer.css";
+
+const InputContainer = () => {
+  return (
+    <div className="d-flex align-items-center justify-content-center input__container"></div>
+  );
+};
+export default InputContainer;
