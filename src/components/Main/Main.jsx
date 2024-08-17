60865ef71ae51ee966acd6e96e2937bc557befa7
Agata Kamińska <a.fluffypie@gmail.com>
2024-08-17 17:59:23 +0200

    feat: introduced logic for the Main component, added click handling and state management for child components, passed props for components, imported the fetchExchangeRate function responsible for retrieving data from the API

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,63 @@
+import { useState } from "react";
 import "./Main.css";
 import ProjectContainerLight from "../ProjectContainerLight/ProjectContainerLight";
 import SpinnerContainer from "../SpinnerContainer/SpinnerContainer";
 import ProjectContainer from "../ProjectContainer/ProjectContainer";
+import { fetchExchangeRate } from "../../services/fetchExchangeRate";
+
 const Main = () => {
+  const [activeCurrency, setActiveCurrency] = useState("USD");
+  const [inputValue, setInputValue] = useState("");
+  const [convertedValue, setConvertedValue] = useState("0,00");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleButtonClick = (currency) => {
+    setActiveCurrency(currency);
+    setConvertedValue("0,00");
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleConversion = async () => {
+    const amount = parseFloat(inputValue);
+    if (!activeCurrency || isNaN(amount) || amount <= 0.009) {
+      alert("Wybierz walutę i wprowadź poprawną kwotę.");
+      return;
+    }
+
+    setIsLoading(true);
+    try {
+      const rate = await fetchExchangeRate(activeCurrency);
+      if (rate) {
+        const result = (rate * amount).toFixed(2).replace(".", ",");
+        setConvertedValue(result);
+      } else {
+        alert("Nie można pobrać kursu dla wybranej waluty.");
+      }
+    } catch (error) {
+      alert(
+        "Wystąpił błąd podczas pobierania danych. Spróbuj ponownie później."
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <main className="d-flex justify-content-center main__container">
-      <ProjectContainerLight />
-      <SpinnerContainer />
-      <ProjectContainer />
+      <ProjectContainerLight convertedValue={convertedValue} />
+      <SpinnerContainer isLoading={isLoading} />
+      <ProjectContainer
+        activeCurrency={activeCurrency}
+        onButtonClick={handleButtonClick}
+        inputValue={inputValue}
+        onInputChange={handleInputChange}
+        onConvert={handleConversion}
+      />
     </main>
   );
 };
+
 export default Main;
