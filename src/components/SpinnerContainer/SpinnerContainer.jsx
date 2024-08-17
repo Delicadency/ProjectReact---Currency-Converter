e291773d763310b762564f68a434c28815eacf29
Agata Kamińska <a.fluffypie@gmail.com>
2024-08-17 16:41:18 +0200

    feat: changed component to change behavior when loading data

--- src/components/SpinnerContainer/SpinnerContainer.jsx
+++ src/components/SpinnerContainer/SpinnerContainer.jsx
@@ -1,6 +1,19 @@
 import "./SpinnerContainer.css";
 
-const SpinnerContainer = () => {
-  return <section id="spinnerContainer"></section>;
+const SpinnerContainer = ({ isLoading }) => {
+  return (
+    <section
+      id="spinnerContainer"
+      className={`${
+        isLoading
+          ? "d-flex justify-content-center align-items-center project-body__container--spinner"
+          : ""
+      }`}
+    >
+      {isLoading && (
+        <div className="spinner-border spinner text-light" role="status"></div>
+      )}
+    </section>
+  );
 };
 export default SpinnerContainer;
